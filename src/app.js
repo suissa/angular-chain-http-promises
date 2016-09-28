c314e81965fdc152f21b592c277ff06a86c175ca
Carlos A. Ferrari <caferrari@gmail.com>
2016-09-28 02:40:29 -0300

    Ajuste no exemplo, melhor uso do finally

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ angular.module('myApp', [])
 .service('CEPService', CEPService)
 .controller('MainCtrl', MainCtrl);
 
-function MainCtrl ($scope, $http, CEPService) {
+function MainCtrl ($scope, $q, CEPService) {
 
 /**
 Mostrando as 2 formas onde cada resposta é enviada diretamente ao $scope
@@ -10,38 +10,51 @@ e usando um Array externo que é populado a cada promise
 e na depois de todas ocorrerem uso o finally para jogar o Array p/ o $scope
 */
 
-  var enderecos = []
-  $scope.error = false
-  CEPService.find('06608430')
-  .then((json) => {
-    $scope.part1 = json.data; 
-    enderecos.push(json.data);
-    return CEPService.find('80610905');
-  })
-  .then((json) => {
-    $scope.part2 = json.data; 
-    enderecos.push(json.data);
-    return CEPService.find('80510170');
-  })
-  .then((json) => {
-    $scope.part3 = json.data; 
-    enderecos.push(json.data);
-  })
-  .catch(function(err) {
-    $scope.errorMessage = 'Deu merda: ' + err.toString();
-    $scope.error = true
-  })
-  .finally(() => {
-    if(!$scope.error) $scope.enderecos = enderecos;
-  });
-}
-MainCtrl['$inject'] = ['$scope', '$http', 'CEPService']
+  $scope.enderecos = [];
+  var ceps = ['06608430', '80610905', '80510170'];
 
-function CEPService ($http) {
-  const BASE_URL = 'http://labs.edysegura.com/busca-por-cep/cep/endereco.php?cep='
-  
-  this.find = function (cep) {
-    return $http.get(BASE_URL + cep)
-  }
+  $scope.error = false
+
+  // habilitamos o estado de "carregando!";
+  $scope.loading = true
+
+  // Criamos uma promise com todas as promises de busca de cep
+  $q.when(ceps.map(CEPService.find)).then(function(promises) {
+    // esperamos todas elas serem resolvidas
+    return $q.all(promises)
+  }).then(function(enderecos) {
+    // adicionamos um campo "completo" na resposta, com o endereço formatado
+    return enderecos.map(function(e) {
+      e.completo = [e.logradouro, e.bairro, e.localidade, e.uf].join(", ");
+      return e;
+    })
+  }).then(function(enderecos) {
+    // definimos os enderecos no escopo
+    $scope.enderecos = enderecos
+  }).catch(function() {
+    // se der erro em algum lugar acima, cai aqui
+    $scope.errorMessage = 'Deu merda: ' + err.toString()
+  }).finally(function() {
+    // removemos o estado de "carregando" =)
+    $scope.loading = false
+  });
+
 }
-CEPService['$inject'] = ['$http']
+MainCtrl['$inject'] = ['$scope', '$q', 'CEPService']
+
+function CEPService ($http, $timeout) {
+  const BASE_URL = 'https://viacep.com.br/ws/{cep}/json/'
+
+  this.find = function (cep) {
+    return $http.get(BASE_URL.replace('{cep}', cep)).then(function(res) {
+      return res.data;
+    }).then(function(data) {
+      // só para simular delay
+      return $timeout(function() {
+        return data
+      }, 2000);
+    })
+  };
+
+}
+CEPService['$inject'] = ['$http', '$timeout']
